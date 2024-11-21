6fafc7ed80a3420ab6e78300d744e8e70e42459c
DVTO16210 <david.torres.marin.16@gmail.com>
2024-11-20 21:33:48 -0600

    se agrego un nuevo menu/funcion del codigo el cual contacta a los programadores

---
+++ contactarProgramadores.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("Se selecciono 'Contactar Programadores'\n");
+    printf("\tBanca SAT S.A de C.V\n\n");
+
+    printf("\tPrograma Realizado por:\n\n");
+    printf("- Espinoza Tello Joan Manuel\n");
+    printf("- Hernandez Herrera Santiago\n");
+    printf("- Torres Marin David Emiliano\n");
+    printf("\n");
+    
+    
+    printf("\tCorreos Electronicos de Contacto:\n\n");
+    printf("joanmet89@gmail.com\n");
+    printf("santyherre10@gmail.com\n");
+    printf("david.torres.marin.16@gmail.com\n");
+    printf("\n");
+
+    
+    printf("\tNo te pierdas de nestras promociones exlusivas por ser cliente\n\n");
+    printf("1.Cashback en tus compras diarias:\n");
+    printf("- Obtén el 5%% de devolucion al pagar con tu\n");
+    printf("  tarjeta en supermercados, restaurantes\n");
+    printf("  y tiendas online.\n");
+    printf("- Promocion valida hasta el 31 de diciembre de 2024.\n");
+    printf("\n");
+    
+    printf("2. Viajes y experiencias inolvidables:\n");
+    printf("- Gana 2 boletos de avion gratis por cada\n");
+    printf("  $15,000 MXN gastados acumulados con tu\n");
+    printf("  tarjeta Platinum.\n");
+    printf("- Participa hasta el 15 de enero de 2025.\n");
+	printf("\n");
+
+    printf("Gracias por confiar en Banca SAT\n");
+    return 0;
+}
+
+
+
+
